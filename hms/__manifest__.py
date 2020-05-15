51e08b3b889affc3adfd796a561c805cec633060
Nouran-yehia <nourayehia1994@gmail.com>
2020-05-15 05:07:25 +0200

    adding Lab1 for odoo

---
+++ hms/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "hms module",
+    "summary": "Hospital Managment System module",
+    "author": "Nouran",
+    "data":[
+        "views/hms_doctor_views.xml",
+        "views/hms_patient_views.xml",
+        "views/hms_department_views.xml",
+        "views/hms_customer_views.xml"
+    ]
+}
